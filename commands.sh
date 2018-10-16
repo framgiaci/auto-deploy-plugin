607eae266a5e19ef10170f06431424e5632b4f8e
dangminhtruong <xdangminhtruongx@gmail.com>
2018-10-16 16:38:03 +0700

    remove chmod authkey

--- commands.sh
+++ commands.sh
@@ -7,5 +7,5 @@ chmod 600 /root/.ssh/id_rsa
 chmod 644 /root/.ssh/id_rsa.pub
 cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys
 echo -e "Host *\n\tStrictHostKeyChecking no\n\tIdentityFile /root/.ssh/id_rsa\n\tAddKeysToAgent yes\n" >> /root/.ssh/config
-chmod 644 /root/.ssh/known_hosts
 $PLUGIN_RUN
+echo -e "deploy:\n  comment: false\n  exit_code: $?\n  ignore: false" >> ./.framgia-ci-result.temp.yml
